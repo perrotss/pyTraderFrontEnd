47a02080fda9370831258ef254dca6ce5bbb8b2a
perrotss <francescoperrozzi3@gmail.com>
2022-12-13 17:43:07 -0500

    fixed submodule and added landing page

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,55 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+	<div id="app">
+		<router-view :auth="auth" :authenticated="authenticated"></router-view>
+		<tour></tour>
+		<notifications group="loggedIn" position="top right" animation-type="velocity" />
+	</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+	import { mapGetters } from "vuex";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+	import AuthService from "./auth/AuthService";
+	import Tour from "Components/Tour/Tour";
+	import AppConfig from "Constants/AppConfig";
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+	const auth = new AuthService();
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+	const { login, logout, authenticated, authNotifier } = auth;
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+	export default {
+		components: {
+			Tour
+		},
+		mounted() {
+			setTimeout(() => {
+		      this.loading = false;
+		      setTimeout(() => {
+		        if (AppConfig.enableUserTour) {
+		          this.$tours["vuelyTour"].start();
+		        }
+		      }, 1000);
+		    }, 2000);
+		},
+		data() {
+			authNotifier.on("authChange", authState => {
+				this.authenticated = authState.authenticated;
+			});
+			return {
+				auth,
+				authenticated,
+				animation: {
+					enter: {
+						opacity: [1, 0],
+						translateX: [0, -300],
+						scale: [1, 0.2]
+					},
+					leave: {
+						opacity: 0,
+						height: 0
+					}
+				}
+			};
+		}
+	};
+</script>
---
+++ src/views/landing/LandingPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="dashboard-button">
+        <router-link :to="'/dashboard'">
+            <v-btn style="background-color: #69C0F7">
+                Dashboard
+            </v-btn>
+        </router-link>
+    </div>
+</template>
+
+<script>
+    import { RouterLink } from 'vue-router';
+    
+    export default {
+    name:'landing',
+    components: { RouterLink },
+
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<style>
+.dashboard-button {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
